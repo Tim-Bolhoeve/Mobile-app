03593485f5ffa84e9cad51a310f4ce98bccc1ea2
Tim <74612255+Tim-Bolhoeve@users.noreply.github.com>
2023-08-23 15:52:30 +0200

    Add files via upload

---
+++ components/EmojiList.js
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { StyleSheet, FlatList, Image, Platform, Pressable } from 'react-native';
+
+export default function EmojiList({ onSelect, onCloseModal }) {
+  const [emoji] = useState([
+    require('../assets/images/nerd.jpg'),
+  ]);
+
+  return (
+    <FlatList
+      horizontal
+      showsHorizontalScrollIndicator={Platform.OS === 'web'}
+      data={emoji}
+      contentContainerStyle={styles.listContainer}
+      renderItem={({ item, index }) => {
+        return (
+          <Pressable
+            onPress={() => {
+              onSelect(item);
+              onCloseModal();
+            }}>
+            <Image source={item} key={index} style={styles.image} />
+          </Pressable>
+        );
+      }}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  listContainer: {
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginRight: 20,
+  },
+});
